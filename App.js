f534f58ef3b0ebf384df9037d1f48a7196c7f68b
Yasir12598 <79859258+Yasir12598@users.noreply.github.com>
2022-03-19 14:39:25 +0500

    u

--- App.js
+++ App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-
+  StyleSheet, View
 } from 'react-native';
-import AntDesign from 'react-native-vector-icons/AntDesign'
+import AntDesign from 'react-native-vector-icons/AntDesign';
 import AppText from './components/AppText';
-import { GolbalStyle } from './components/GlobalSyle'
+import { GolbalStyle } from './components/GlobalSyle';
+
 
 
 export default function App() {
   return (
     <View style={GolbalStyle.container}>
       <AntDesign name='forward' size={20} color='black' />
-      <AppText name="komail"> Hellohhhhhhhhhhhhhhh </AppText>
+      <AppText name="komail"> Hellohhhhhhhhhhhhhhhhhhhhhh </AppText>
 
     </View>
 
